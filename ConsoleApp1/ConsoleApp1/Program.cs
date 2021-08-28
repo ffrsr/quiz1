8a0b933407971eb89e0589b06c8dbb18e9dad632
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-08-28 09:46:02 +0700

    no message

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 0;
+            x = double.Parse(Console.ReadLine());
+
+            if ( x == 1  || x == 2)
+            {
+                y = 2 * x;
+            }
+            else if ( x>=5 && x<=8)
+            {
+                y = (3 * x) + 2;
+            }
+            else if (x==3)
+            {
+                y = Math.Sqrt(x);
+            }
+            else if (x>=9 && x<=12)
+            {
+                y = Math.Sqrt(x);
+            }
+
+        }
+    }
+}
